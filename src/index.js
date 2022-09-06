11ab6b0da95fd0d51b8512286dfc98b304808ce4
Jesus Ramirez Perez <polo1-1@live.com.mx>
2022-09-05 23:50:34 -0500

    act

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import {saludar} from './js/componentes.js'
-import './styles.css'
+// 
+// Realizar peticiones HTTP a  un host
+// Se realiza mediante la funcion fetch()  yaes una funcion nativa
+// fetch() regresa una promesa que resuelve el response
+// 
 
-const nombre = 'jesus';
-saludar(nombre);
+// Codigos de error en peticiones HTTP
+
+const urlHost = 'https://api.chucknorris.io/jokes/random';
+
+fetch( urlHost ).then( resp => {            // resp --> respuesta de nuestra peticion HTTP
+  console.log(resp);
+  resp.json().then(data => {                // Datos de nuestro responce 
+    console.log(data);
+  });
+});
